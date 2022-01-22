a59cf97fca603a7259cbf0ecd40a89f4c532654f
Garrett Gunnell <gunnellg@oregonstate.edu>
2022-01-22 00:10:47 -0800

    Change global background color

--- src/Home.svelte
+++ src/Home.svelte
@@ -4,22 +4,14 @@
 
 <style>
 	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
+		width: 100%;
+		height: 100%;
+		background-color: #a4958e;
 	}
 
 	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
+		color: #FFFFFF;
 		font-size: 4em;
 		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
 	}
 </style>
